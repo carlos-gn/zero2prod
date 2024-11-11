ce2088eba4d3611953aaf534bd78b3a28511884c
Carlos <carlosgn@protonmail.com>
2024-11-11 21:52:22 +0100

    add confirmation endpoint

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -2,17 +2,44 @@ use secrecy::SecretString;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::sync::LazyLock;
 use uuid::Uuid;
+use wiremock::MockServer;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Appplication};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+}
+
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
     pub client: reqwest::Client,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
+    pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+        // Extract the link from one of the request fields.
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+            assert_eq!(links.len(), 1);
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+            // Let's make sure we don't call random APIs on the web
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+        let html = get_link(&body["html"].as_str().unwrap());
+        ConfirmationLinks { html }
+    }
+
     pub async fn post_subscriptions(&self, body: &'static str) -> reqwest::Response {
         self.client
             .post(&format!("{}/subscriptions", &self.address))
@@ -32,10 +59,13 @@ static TRACING: LazyLock<()> = LazyLock::new(|| {
 pub async fn spawn_app() -> TestApp {
     LazyLock::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -44,16 +74,17 @@ pub async fn spawn_app() -> TestApp {
     let application = Appplication::build(configuration.clone())
         .await
         .expect("Failed to build application");
-
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
     tokio::spawn(application.run_until_stopped());
 
     let client = reqwest::Client::new();
 
     TestApp {
-        address,
+        address: format!("http://localhost:{}", application_port),
+        port: application_port,
         client,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
